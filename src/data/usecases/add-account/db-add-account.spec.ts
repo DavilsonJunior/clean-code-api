cf2ae2598aba5db483dba83858b53ed67e3365cb
Davilson Paulino da Cunha Junior <davilson.junior@stitcloud.com>
2023-11-18 18:23:44 -0300

    refactor: group all protocols in a single file

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,7 +1,5 @@
-import { resolve } from 'path'
-import { Encrypter } from '../../protocols/encrypter'
 import { DbAddAccount } from './db-add-account'
-import { rejects } from 'assert'
+import { Encrypter } from './db-add-account-protocols'
 
 interface SutTypes {
   sut: DbAddAccount
